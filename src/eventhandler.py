f4f38f0eb54505eb33f2d6b7845d544dc9957adf
Gabriel Wong <me@gabrielwong.net>
2014-01-25 05:31:32 -0500

    Add tap and double tap gesture functionality

--- src/eventhandler.py
+++ src/eventhandler.py
@@ -11,6 +11,12 @@ MOTION_DOWN = 0
 MOTION_MOVE = 1
 MOTION_UP = 2
 
+GESTURE_TAP = 0
+GESTURE_DOUBLE_TAP = 1
+GESTURE_LONG_PRESS = 2
+GESTURE_SCROLL = 3
+GESTURE_PINCH = 4
+
 class EventHandler:
     m = PyMouse()
     k = PyKeyboard()
@@ -38,31 +44,43 @@ class EventHandler:
         if gesture != None:
             gestureType = gesture.get("type")
             if gestureType != None:
-                s_x, s_y = self.m.position()
-                self.performGesture(type, s_x, s_y)
-        else: # Otherwise process pointer movement
-            pointer = pointers[0]
+                self.performGesture(type)
+                return
+        # Otherwise process pointer movement
+        pointer = pointers[0]
+        
+        # Store initial coordinates
+        if pointer["motionEvent"] == MOTION_DOWN:
+            self.a_first_x = pointer["x"]
+            self.a_first_y = pointer["y"]
+            self.s_first_x, self.s_first_y = self.m.position()
             
-            # Store initial coordinates
-            if pointer["motionEvent"] == MOTION_DOWN:
-                self.a_first_x = pointer["x"]
-                self.a_first_y = pointer["y"]
-                self.s_first_x, self.s_first_y = self.m.position()
-                
-            # Move the mouse
-            elif pointer["motionEvent"] == MOTION_MOVE:
-                s_new_x, s_new_y = self.newMoveScreenPosition(pointer["x"], pointer["y"])
-                print "Moving cursor to x:{x}, y:{y}".format(x=s_new_x, y=s_new_y)
-                self.m.move(s_new_x, s_new_y)
+        # Move the mouse
+        elif pointer["motionEvent"] == MOTION_MOVE:
+            s_new_x, s_new_y = self.newMoveScreenPosition(pointer["x"], pointer["y"])
+            print "Moving cursor to x:{x}, y:{y}".format(x=s_new_x, y=s_new_y)
+            self.m.move(s_new_x, s_new_y)
 
     # Process Gestures
-    def performGesture(self, type, prev_x, prev_y):
-        if type == 0:
-            print "Primary click x:{x}, y:{y}".format(x=prev_x, y=prev_y)
-            self.m.click(prev_x, prev_y, button=1)
-        elif type == 1:
-            print "Right click x:{x}, y:{y}".format(x=prev_x, y=prev_y)
-            self.m.click(prev_x, prev_y, button=2)
+    def performGesture(self, type, pointers):
+        s_x, s_y = self.m.position()
+        if type == GESTURE_TAP:
+            # Primary click if one pointer
+            if len(pointers) == 1:
+                print "Primary click x:{x}, y:{y}".format(x=s_x, y=s_y)
+                self.m.click(s_x, s_y, button=1)
+            # Secondary click if two pointers
+            elif len(pointers) == 2:
+                print "Secondary click x:{x}, y:{y}".format(x=s_x, y=s_y)
+                self.m.click(s_x, s_y, button=2)
+            # Middle click if three pointers
+            elif len(pointers) == 3:
+                print "Middle click x:{x}, y:{y}".format(x=s_x, y=s_y)
+                self.m.click(s_x, s_y, button=3)
+        elif type == GESTURE_DOUBLE_TAP:
+            # There is a preceding single tap so we only need to send one more for a double tap
+            print "'Double' click x:{x}, y:{y}".format(x=s_x, y=s_y)
+            self.m.click(s_x, s_y, button=1)
     
     # Calculates the new screen position
     def newMoveScreenPosition(self, a_x, a_y):
