954ced341670292a2021f6e240f39988bb03e62d
Gabriel Wong <me@gabrielwong.net>
2014-01-25 01:20:52 -0500

    Added socket wrapper from python lib

--- src/main.py
+++ src/main.py
@@ -5,19 +5,22 @@ Created on Jan 24, 2014
 '''
 
 import socket.tcpbluetooth
+import socketfile._fileobject
 
 def main():
     connection = socket.tcpbluetooth.connect()
     if connection == None:
         return
     
-    f = connection.makefile("r+b", buffersize=0)
+    f = makefile(socket)
     while True:
         print f.readline()
     
     #with open(connection, "rw") as f:
      #   print f.readline()
     
+def makefile(socket, mode='r+b', bufsize=0):
+    return socketfile._fileobject(socket, mode, bufsize)
 
 if __name__ == '__main__':
     main()
